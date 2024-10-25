548919dadae63c83ff18f8eb278678f2b78c7b20
bee-honey <honeybeeprogrammer@gmail.com>
2024-10-25 03:32:24 -0700

    Add existing project files to Git

---
+++ KeerthyAbbot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeerthyAbbot
+//
+//  Created by Naveen Keerthy on 10/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Abbot!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeerthyAbbot/KeerthyAbbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeerthyAbbotApp.swift
+//  KeerthyAbbot
+//
+//  Created by Naveen Keerthy on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct KeerthyAbbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
